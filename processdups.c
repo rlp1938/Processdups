5fef899b6452f4d706fdde5004739588448e1664
bob <rlp1938@gmail.com>
2015-10-12 20:39:26 +0700

    Updated copyright date.

--- processdups.c
+++ processdups.c
@@ -1,7 +1,7 @@
 /*
  * processdups.c
  *
- * Copyright 2014 Bob Parker <rlp1938@gmail.com>
+ * Copyright 2015 Bob Parker <rlp1938@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
